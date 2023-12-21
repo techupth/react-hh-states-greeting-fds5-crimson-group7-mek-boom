2ca1ea8e069097a0e98c3a6adc09041609d68347
Nitisit Vithunat <nitisitvithunat@MacBook-Pro-khxng-Nitisit.local>
2023-12-22 02:49:22 +0700

    feat:event on click (button)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 import "./App.css";
+import { useState } from "react";
+
+//มี state 1 อันชื่อ greeting massage
+//มี event click
 
 function App() {
+  //สร้าง useState
+  const [greetingMassage, setGreetingMassage] = useState("Greeting Message"); //ฟังชั่นนี้ return array คือ[state,fucntion ที่เอาไว้เปลี่ยน state (เพื่อ re-render)]
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMassage}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button
+          onClick={() => {
+            setGreetingMassage("สวัสดี!"); //ใช้ callback fn. เพื่อre-render state โดยอ่้างถึง paramiter ใน state(1) หรือก็คือตัวที่ 2
+          }}
+        >
+          สวัสดี!
+        </button>
+        <button
+          onClick={() => {
+            setGreetingMassage("Hi!");
+          }}
+        >
+          Hi!
+        </button>
+        <button
+          onClick={() => {
+            setGreetingMassage("你好!");
+          }}
+        >
+          你好!
+        </button>
       </div>
     </div>
   );
